508fb75f043a5dabb061e7e622ee8ab97be0c674
git_rtk <17_ufo_RB@proton.me>
2024-04-04 19:01:02 +0200

    add test_filtered_fin_info

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -9,3 +9,12 @@ file = os.path.join(ROOT_DIR, 'src', 'operations.json')
 
 def test_upload_data():
     assert upload_data(file)
+
+
+def test_filtered_fin_info():
+    cor_inputs = [{"state": "EXECUTED"}]
+    empty_inputs = [{}]
+    noncor_inputs = [{"state": "CANCELED"}]
+    assert filtered_fin_info(cor_inputs) == cor_inputs
+    assert filtered_fin_info(empty_inputs) == []
+    assert filtered_fin_info(noncor_inputs) == []
